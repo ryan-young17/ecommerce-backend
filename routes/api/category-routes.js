9d127b455fb2dc704420f8ff421e6ebbf4b305b3
Ryan Young <ryanbyoung1@gmail.com>
2023-01-12 18:56:47 -0500

    added in functions to product-routes file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -81,6 +81,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'No category can be found with that id!'});
       return;
     }
+    
     res.status(200).json(categoryData);
   } catch (err) {
     console.log(err);
